dffc7af3dde9da8860c3980fcb014e4459a005f1
Ren <rnaka530@yahoo.com>
2014-02-12 00:31:18 -0800

    Update MyLocation.java
    updated support for fetching time/date.

--- MyLocation.java
+++ MyLocation.java
@@ -1,5 +1,7 @@
 package com.rennakanote.VIS141Final;
 
+import java.sql.Date;
+
 import android.app.Activity;
 import android.content.Context;
 import android.location.Criteria;
@@ -7,33 +9,38 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-
 public class MyLocation extends Activity implements LocationListener  {
+	  
 	  private TextView latituteField;
 	  private TextView longitudeField;
+	  private TextView dateField;
 	  private LocationManager locationManager;
 	  private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		latituteField = (TextView) findViewById(R.id.latTextView);
 	    longitudeField = (TextView) findViewById(R.id.lngTextView);
+	    dateField = (TextView) findViewById(R.id.timeTextView);
+	    
 	    // Get the location manager
 	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    // Define the criteria how to select the location provider -> use
-	    // default
+
+	    // Define the criteria how to select the location provide
 	    Criteria criteria = new Criteria();
 	    provider = locationManager.getBestProvider(criteria, false);
 	    Location location = locationManager.getLastKnownLocation(provider);
-
+	    // fetch current date
+	    Date date = new Date(location.getTime());
+	    java.text.DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+	    
 	    // Initialize the location fields
 	    if (location != null) {
 	      System.out.println("Provider " + provider + " has been selected.");
@@ -42,16 +49,17 @@ public class MyLocation extends Activity implements LocationListener  {
 	    } else {
 	      latituteField.setText("Location not available");
 	      longitudeField.setText("Location not available");
+	      dateField.setText("Timelog not available");
 	    }
 	  }	    
-	  /* Request updates at startup */
+	  // Request updates at startup
 	  @Override
 	  protected void onResume() {
 	    super.onResume();
 	    locationManager.requestLocationUpdates(provider, 400, 1, this);
 	  }
 
-	  // Remove the locationlistener updates when Activity is paused
+	  // Remove the location listener updates when Activity is paused
 	  @Override
 	  protected void onPause() {
 	    super.onPause();
