62eb4d2d8b42b142f4ed9f783cbbd67a700b8010
Ren <rnaka530@yahoo.com>
2014-02-16 12:05:35 -0800

    Update DatabaseHandler.java

--- DatabaseHandler.java
+++ DatabaseHandler.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHandler extends SQLiteOpenHelper  {
 	
@@ -25,6 +26,9 @@ public class DatabaseHandler extends SQLiteOpenHelper  {
 	private static final String KEY_ID = "ID";
 	private static final String KEY_LAT = "LAT"; 
 	private static final String KEY_LNG = "LNG";
+
+	public String latitudeField;
+	public String longitudeField;
 	
 	public DatabaseHandler(Context context)  {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -35,7 +39,10 @@ public class DatabaseHandler extends SQLiteOpenHelper  {
 	public void onCreate(SQLiteDatabase db)  {
 		String CREATE_CORDINATES_TABLE = "CREATE TABLE " + TABLE_CORDINATES + "("
 				+ KEY_ID + "INTEGER PRIMARY KEY, " + KEY_LAT + " TEXT,"  
-				+ "TEXT," + KEY_LNG + ")" ;
+				+ "TEXT," + KEY_LNG + ")";
+		
+		// create table
+		db.execSQL(CREATE_CORDINATES_TABLE);
 	}
 	
 	// Upgrading database
@@ -44,55 +51,80 @@ public class DatabaseHandler extends SQLiteOpenHelper  {
 		db.execSQL("DROP TABLE IF EXISTED" + TABLE_CORDINATES);
 		
 		// Create table again
-		onCreate(db);
+		this.onCreate(db);
 	}
 	
 	//Adding new Coordinate
-	void addCordinate(Cordinates cordinate)  {
+	public void addCordinate(Cordinates cordinate)  {
+		// Print to the log cat
+		Log.println(Log.ASSERT, "addCordinate", cordinate.toString());
+		// 1. Get reference to writable DB
 		SQLiteDatabase db = this.getWritableDatabase();
 		
+		// 2. Create contentValues to add key "column" / value
 		ContentValues values = new ContentValues();
 		values.put(KEY_LAT, cordinate.getLat()); // Coordinates Lat
 		values.put(KEY_LNG, cordinate.getLng()); // Coordinates Lng
 		
-		// Inserting a Row
+		// 3. Inserting a Row
 		db.insert(TABLE_CORDINATES, null, values);
+		// 4. Close the database
 		db.close(); // close database connection
 	}
 	
 	// Getting Single Cordinate Row
 	Cordinates getCordinate(int id)  {
+		// 1. get reference to readable DB
 		SQLiteDatabase db = this.getReadableDatabase();
 		
-		Cursor cursor = db.query(TABLE_CORDINATES, new String[] { KEY_ID, KEY_LAT, KEY_LNG }, KEY_ID + "=?", 
-				new String[] {String.valueOf(id) }, null, null, null, null);
+		// 2. Build query
+		Cursor cursor = db.query(TABLE_CORDINATES, 			 // a. table
+				new String[] { KEY_ID, KEY_LAT, KEY_LNG },   // b. column names 
+				KEY_ID + "=?", 								 // c. selections 
+				new String[] {String.valueOf(id) },     	 // d. selection args  
+				null, 										 // e. group by
+				null, 										 // f. having
+				null,  										 // g. order by
+				null); 										 // h. limit
+		
+		// 3.  if we get results, get the first one 
 		if(cursor != null)
 			cursor.moveToFirst();
 		
+		// 4.  build cordinate object
 		Cordinates cordinate = new Cordinates();
+		
+		// Print to the log cat
+		Log.println(Log.ASSERT,"getCordinates("+id+")", cordinate.toString());
+		// 5.  return cordinates
 		return cordinate;
 	}
 	
 	// Getting All Cordinates
 	public List<Cordinates> getAllCordinates()  {
 		List<Cordinates> cordinateList = new ArrayList<Cordinates>();
-		// Select All Query
+		// 1.  Build the query for Select All
 		String selectQuery = "SELECT *  FROM * " + TABLE_CORDINATES;
 		
+		// 2.  get reference to writable DB
 		SQLiteDatabase db = this.getWritableDatabase();
 		Cursor cursor = db.rawQuery(selectQuery, null);
 		
-		// looping through all rows and adding to list
+		
+		// 3.  Looping through all rows and adding to list
 		if (cursor.moveToFirst())  {
 			do {
 				Cordinates cordinates = new Cordinates();
 				cordinates.setID(Integer.parseInt(cursor.getString(0)));
 				cordinates.setLat(cursor.getLong(1));
 				cordinates.setLng(cursor.getLong(2));
+				
 				// Adding Cordinate to list
 				cordinateList.add(cordinates);
 			} while (cursor.moveToNext());
 		}
+		
+		Log.println(Log.ASSERT,"getAllCordinates()", cordinateList.toString());
 		// return the cordinate list
 		return cordinateList;
 	}
@@ -104,8 +136,8 @@ public class DatabaseHandler extends SQLiteOpenHelper  {
 		SQLiteDatabase db = this.getWritableDatabase();
 		
 		ContentValues value = new ContentValues();
-		value.put(KEY_LAT, cordinate.getLat());
-		value.put(KEY_LNG, cordinate.getLng());
+		value.put(KEY_LAT, cordinate.getLat()); // get latitude
+		value.put(KEY_LNG, cordinate.getLng()); // get longitude
 		
 		// Updating row
 		return db.update(TABLE_CORDINATES, value, KEY_ID + " = ?",
@@ -114,10 +146,16 @@ public class DatabaseHandler extends SQLiteOpenHelper  {
 	
 	// Deleting a single Coordinate
 	public void deleteCordinate(Cordinates cordinate)  {
+		// 1.  get reference to writable DB
 		SQLiteDatabase db = this.getWritableDatabase();
+		
+		// 2.  delete
 		db.delete(TABLE_CORDINATES, KEY_ID + "=?",
 				new String[] {String.valueOf(cordinate.getID()) });
+		
+		// 3. Close
 		db.close();
+		Log.println(Log.ASSERT, "deleteCordinate", cordinate.toString());
 	}
 	
 	// Getting Cordinate total count
@@ -127,6 +165,8 @@ public class DatabaseHandler extends SQLiteOpenHelper  {
 		Cursor cursor = db.rawQuery(countQuery, null);
 		cursor.close();
 		
+		Log.println(Log.ASSERT, "getcCordinatesCount()", countQuery.toString());
+
 		// return count
 		return cursor.getCount();
 	}
