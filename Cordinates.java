c5942e8d6d02e6254e5f6ab677b11d595dd909d9
Ren <rnaka530@yahoo.com>
2014-02-23 06:20:03 -0800

    Update Cordinates.java
    Added getTime()

--- Cordinates.java
+++ Cordinates.java
@@ -1,55 +1,62 @@
-package com.rennakanote.VIS141Final;
+package com.rennakanote.gpsdraw;
 
 public class Cordinates {
-	
-	// private variables
-	int _id;
-	float _latValue;
-	float _lngValue;
-	
-	// Empty Constructor
-	public Cordinates() { 
-	
-	}
-	
-	// constructor
-	public Cordinates(int id, float latValue, float lngValue)  {
-		this._id = id;
-		this._latValue = latValue;
-		this._lngValue = lngValue;
-	}
-	/* GETTERS for id, latitude, longitude
-	 * Will try to add the import for time later...
-	 */
-	
-	// getting ID
-	public int getID()  {
-		return this._id;
-	}
-	// getting latitude
-	public float getLat()  {
-		return this._latValue;
-	}
-	// getting longitude
-	public float getLng()  {
-		return this._lngValue;
-	}
-	
-	/* SETTERS for id, latitude, longitude
-	 * Will try to add the import for time later...
-	 */	
-	// setting id
-	public void setID(int id)   {
-		this._id = id;
-	}
-	
-	// setting latitude
-	public void setLat(long latValue)  {
-		this._latValue = latValue;
-	}
-	
-	// setting longitude
-	public void setLng(long lngValue)  {
-		this._lngValue = lngValue;
-	}
+
+    // private variables
+    int _id;
+    float _timeValue;
+    float _latValue;
+    float _lngValue;
+
+    // Empty Constructor
+    public Cordinates() { 
+
+    }
+
+    // constructor
+    public Cordinates(int id, float timeValue, float latValue, float lngValue)  {
+        this._id = id;
+        this._timeValue = timeValue;
+        this._latValue = latValue;
+        this._lngValue = lngValue;
+    }
+    /* GETTERS for id, latitude, longitude
+     * Will try to add the import for time later...
+     */
+
+    // getting ID
+    public int getID()  {
+        return this._id;
+    }
+    // getting time
+    public float getTime()  {
+    	return this._timeValue;
+    }
+    // getting latitude
+    public float getLat()  {
+        return this._latValue;
+    }
+    // getting longitude
+    public float getLng()  {
+        return this._lngValue;
+    }
+
+    /* SETTERS for id, latitude, longitude
+     * Will try to add the import for time later...
+     */ 
+    public void setID(int id)   {
+        this._id = id;
+    }
+    public void setTime(long timeValue)  {
+    	this._timeValue = timeValue;
+    }
+    // setting latitude
+    public void setLat(long latValue)  {
+        this._latValue = latValue;
+    }
+
+    // setting longitude
+    public void setLng(long lngValue)  {
+        this._lngValue = lngValue;
+    }   
 }
